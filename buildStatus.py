f02e8edff58161a3273c2ac2979bcd8e1f87c502
lena.feygin <lena.feygin@webcollage.com>
2018-01-27 15:28:10 +0200

    changed copy location

--- buildStatus.py
+++ buildStatus.py
@@ -18,15 +18,40 @@ def urlopen(url, data=None):
     response = urllib2.urlopen(request)
     return response
 
+def convertToTime(milliseconds):
+    milliseconds = int(milliseconds)
+    seconds=(milliseconds/1000)%60
+    seconds = int(seconds)
+    minutes=(milliseconds/(1000*60))%60
+    minutes = int(minutes)
+    hours=(milliseconds/(1000*60*60))%24
+    return "%02d:%02d:%02d" % (hours, minutes, seconds)
 
 
-if len( sys.argv ) > 1 :
+
+if len( sys.argv ) > 2 :
     jobName = sys.argv[1]
+    buildNumber = sys.argv[2]
 else :
+    print "One of the parameters is missing. Run: 'python buildStatus.py jobName buildNumber'"
     sys.exit(1)
 
+# Distnguish between pipeline and other jobs
 try:
-    jenkinsStream   = urlopen(jenkinsUrl + jobName + "/lastBuild/api/json")
+    jenkinsStream   = urlopen(jenkinsUrl + jobName + "/api/json)
+except urllib2.HTTPError, e:
+    print "URL Error: " + str(e.code) 
+    print "      (job name [" + jobName + "] probably wrong)"
+    sys.exit(2)
+
+try:
+    jobJson = json.load( jenkinsStream )
+except:
+    print "Failed to parse json"
+    sys.exit(3)
+
+try:
+    jenkinsStream   = urlopen(jenkinsUrl + jobName + "/"+ buildNumber + "/api/json")
 except urllib2.HTTPError, e:
     print "URL Error: " + str(e.code) 
     print "      (job name [" + jobName + "] probably wrong)"
@@ -38,11 +63,24 @@ except:
     print "Failed to parse json"
     sys.exit(3)
 
-if buildStatusJson.has_key( "result" ):      
-    print "[" + jobName + " #" + str(buildStatusJson["number"]) + "]: " + buildStatusJson["result"] 
-    if buildStatusJson["result"] != "SUCCESS" :
-		exit(4)
+print "Job \"" + jobName + "\" build #" + buildNumber + ":"
+for s in buildStatusJson["actions"]:
+    if s.has_key("causes"):
+        for cause in s["causes"]:
+            print (cause["shortDescription"])
+
+if buildStatusJson.has_key("result"):      
+    print "Job Status: " + buildStatusJson["result"] 
 else:
-	sys.exit(5)
+    print "No status found"
+
+if buildStatusJson.has_key("duration"):      
+    print "Duration: " + convertToTime(buildStatusJson["duration"])
+else: 
+    print "No duration found"
+
+if buildStatusJson.has_key("builtOn"):
+    print "Slave: " + (buildStatusJson["builtOn"] if buildStatusJson["builtOn"] != "" else "master")
+
 
 sys.exit(0)
